41fa7e3976b29176258723c1239bd764e2f68aa9
kmturhan <kmturhan@gmail.com>
2023-06-03 18:46:28 +0300

    feat: configuration in startup file for the localization

--- CarPark/CarPark.User/Startup.cs
+++ CarPark/CarPark.User/Startup.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -24,6 +27,27 @@ namespace CarPark.User
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddControllersWithViews();
+
+			services.AddLocalization(opt => opt.ResourcesPath = "Resources");
+
+			services.Configure<RequestLocalizationOptions>(opt =>
+			{
+				var supportedCultures = new List<CultureInfo>
+				{
+					new CultureInfo("en-US"),
+					new CultureInfo("tr-TR")
+				};
+
+				opt.DefaultRequestCulture = new Microsoft.AspNetCore.Localization.RequestCulture("tr-TR");
+				opt.SupportedCultures = supportedCultures;
+				opt.SupportedUICultures = supportedCultures;
+
+				opt.RequestCultureProviders = new List<IRequestCultureProvider> {
+					new QueryStringRequestCultureProvider(),
+					new CookieRequestCultureProvider(),
+					new AcceptLanguageHeaderRequestCultureProvider()
+				};
+			});
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +69,9 @@ namespace CarPark.User
 			app.UseRouting();
 
 			app.UseAuthorization();
+
+			var options = app.ApplicationServices.GetService<IOptions<RequestLocalizationOptions>>();
+			app.UseRequestLocalization(options.Value);
 
 			app.UseEndpoints(endpoints =>
 			{
