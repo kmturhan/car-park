c511ce84c8b63291294ff05b09c876e3c0112855
kmturhan <kmturhan@gmail.com>
2023-06-07 10:25:47 +0300

    fix: edited structure for connection to the database

---
+++ CarPark/CarPark.Core/Repository/Abstract/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using CarPark.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarPark.Core.Repository.Abstract
+{
+	public interface IRepository<TEntity> where TEntity : class, new()
+	{
+		GetManyResult<TEntity> AsQueryable();
+		Task<GetManyResult<TEntity>> AsQueryableAsync();
+	}
+}
---
+++ CarPark/CarPark.DataAccess/Repository/MongoRepositoryBase.cs
@@ -0,0 +1,10 @@
+﻿using CarPark.Core.Repository.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace CarPark.DataAccess.Repository
+{
+	public class MongoRepositoryBase<TEntity> : IRepository<TEntity> where TEntity : class, new()
+	{
+	}
+}
